0318579616a9222c9068514198fc86c20dc1f7de
Raik Steinkemper <ra.steinkemper@gmail.com>
2026-02-02 19:03:41 +0100

    bug_fix

--- android/app/src/main/kotlin/com/opnv/poc/SessionManager.kt
+++ android/app/src/main/kotlin/com/opnv/poc/SessionManager.kt
@@ -9,7 +9,7 @@ import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
 import java.util.UUID
 
-const val API_BASE = "http://192.168.1.10:3000"  // <- ändere IP hier
+const val API_BASE = "http://192.168.2.146:3000"  // <- ändere IP hier
 
 data class SessionStartRequest(val deviceId: String, val vehicleId: String)
 data class SessionStartResponse(val token: String, val sessionId: String)
